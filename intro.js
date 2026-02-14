d496c5c733f8b59951a396dc30495ece28384f56
JamesMadeIt1 <100561661+JamesMadeIt1@users.noreply.github.com>
2026-02-14 14:46:34 +0000

    Fix variable name typo and log output

--- intro.js
+++ intro.js
@@ -3,8 +3,8 @@ let namee = 'James Bedu-Addo' ;
 let age = 27 ;
 let favorite_programming_language = 'Javascript' ;
 
-let Combiation = 'My name is ' + '' + namee + '.I am ' +  
-age + 'years old' + 'and my favorite programming language is' + 
+let Combination = 'My name is ' + '' + namee + '.I am ' +  
+age + 'years old' + ' '+ 'and my favorite programming language is' + 
 ' ' +  favorite_programming_language ;
 
-console.log(Combiation)
+console.log(Combination)
