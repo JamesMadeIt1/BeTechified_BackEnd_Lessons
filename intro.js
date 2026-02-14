49761a989f3904a1d2f278c9f26fbc315746dc81
JamesMadeIt1 <100561661+JamesMadeIt1@users.noreply.github.com>
2026-02-14 14:39:51 +0000

    intro.js
    BeTechified_Assignment

---
+++ intro.js
@@ -0,0 +1,10 @@
+let namee = 'James Bedu-Addo' ;
+
+let age = 27 ;
+let favorite_programming_language = 'Javascript' ;
+
+let Combiation = 'My name is ' + '' + namee + '.I am ' +  
+age + 'years old' + 'and my favorite programming language is' + 
+' ' +  favorite_programming_language ;
+
+console.log(Combiation)
